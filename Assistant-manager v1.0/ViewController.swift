c60b00546e0affd0c0b7fa3a0c57b0149c5b4064
Khlomov Anton <antonhlomov@yandex.ru>
2022-01-14 11:26:04 +0100

    Initial Commit

---
+++ Assistant-manager v1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assistant-manager v1.0
+//
+//  Created by Anton Khlomov on 14/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
