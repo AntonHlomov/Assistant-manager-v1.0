40a909be88ee02dd7c0d96ddb639144b2b2348af
Khlomov Anton <antonhlomov@yandex.ru>
2022-02-19 17:57:46 +0100

    сделал root controller

---
+++ Assistant-manager v1.0/RegistrationModule/APiRegistration/APiRegistration.swift
@@ -0,0 +1,70 @@
+//
+//  APiRegistration.swift
+//  Assistant-manager v1.0
+//
+//  Created by Anton Khlomov on 19/02/2022.
+//
+
+import UIKit
+import Firebase
+
+
+protocol APIRegistrationProtocol {
+    func registration(photoUser: UIImage, emailAuth: String, name: String, passwordAuth: String,completion: @escaping (Result<Bool,Error>) -> Void)
+}
+
+class APIRegistrationService:APIRegistrationProtocol {
+    func registration(photoUser: UIImage, emailAuth: String, name: String, passwordAuth: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        DispatchQueue.global(qos: .utility).async {
+            
+            Auth.auth().createUser(withEmail: emailAuth, password: passwordAuth) {
+                (user, error) in
+                if let error = error {
+                    print("!!!!!!!Filed  error", error.localizedDescription)
+                    completion(.failure(error))
+                    return
+                }
+                print("Пользователь успешно создан")
+                //качество фото при загрузка в базу данных
+                guard let uploadDataPhoto = photoUser.jpegData(compressionQuality: 0.3)  else {return}
+                //NSUUID() -  это рандомное имя
+                let idPhoto = NSUUID().uuidString
+                //код загрузки фото
+                let storageRef = Storage.storage().reference().child("user_profile_image").child(idPhoto)
+                storageRef.putData(uploadDataPhoto, metadata: nil) { (_, error) in
+                    if let error = error {
+                        print("!!!!!!!Filed error", error.localizedDescription)
+                        completion(.failure(error))
+                        return
+                    }
+                    print("Загрузка фотографии пользователя прошла успешно!")
+                    
+                    //получаем обратно адрес картинки
+                    storageRef.downloadURL { (downLoardUrl, error) in
+                        guard let profileImageUrl = downLoardUrl?.absoluteString else {return}
+                        if let error = error {
+                            print("!!!!!!!Filed error", error.localizedDescription)
+                            completion(.failure(error))
+                            return
+                    }
+                        print("Успешна получина ссылка на картинку")
+                        guard let uid = Auth.auth().currentUser?.uid else {return}
+                        let docData = ["uid": uid,"name": name, "email": emailAuth, "profileImageUrl": profileImageUrl]
+                        Firestore.firestore().collection("users").document(uid).setData(docData) { (error) in
+                            if let error = error {
+                                print("!!!!!!!Filed error", error.localizedDescription)
+                                completion(.failure(error))
+                                return
+                        }
+                            print("Успешна сохранены данные")
+                            // здесь презентер говорит вьюхе(абстрактной) что ей сделать
+                            completion(.success(true))
+                        }
+                      }
+                   }
+                 }
+            
+    
+        }
+    }
+}
